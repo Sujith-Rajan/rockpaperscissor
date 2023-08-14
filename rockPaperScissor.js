0627e69cb97649ac192bc0eb7e31232979ae19ee
Sujith-Rajan <sujithrajan223@gmail.com>
2023-08-14 15:17:46 +0530

    user selection and computer auto selection update and also add score of the players

--- rockPaperScissor.js
+++ rockPaperScissor.js
@@ -1,47 +1,82 @@
-//fetch user input
+//decalration variable for fetch user input______________________________________________________
 const pRock = document.querySelector("#pRock");
 const pScissor = document.querySelector("#pScissor");
 const pPapper = document.querySelector("#pPapper");
 
-//random selection of computer
+//random selection of computer___________________________________________________________________
 const cRock = document.querySelector("#cRock");
 const cScissor = document.querySelector("#cScissor");
 const cPapper = document.querySelector("#cPapper");
-const choice = [cRock,cScissor,cPapper];
+const choice = [cRock, cScissor, cPapper];
 
+// computer auto selection when after player click_______________________________________________
 
- 
-// Animation applied all icon
-pRock.addEventListener(
-    "click",
-    (move = () => {
-        
-        const computerChoice = choice[Math.floor(Math.random()*3)];
-        
+pRock.addEventListener("click", () => playGame("rock"));
+pScissor.addEventListener("click", () => playGame("scissor"));
+pPapper.addEventListener("click", () => playGame("papper"));
+var playerPoint = 0;
+var computerPoint = 0;
+
+function playGame(selection) {
+    // if already any animation active that removed when play game________________________________
+    const computerChoice = choice[Math.floor(Math.random() * 3)];
+    cRock.classList.remove("fa-beat");
+    cPapper.classList.remove("fa-beat");
+    cScissor.classList.remove("fa-beat");
+    pRock.classList.remove("fa-beat");
+    pScissor.classList.remove("fa-beat");
+    pPapper.classList.remove("fa-beat");
+    computerChoice.classList.add("fa-beat");
+
+    if (selection === "rock") {
+        // add animation both player_______________________________________________________________
         computerChoice.classList.add("fa-beat");
-        console.log(computerChoice);
-        pScissor.classList.remove("fa-beat");
-        pPapper.classList.remove("fa-beat");
         pRock.classList.add("fa-beat");
-    })
-);
-pScissor.addEventListener(
-    "click",
-    (move = () => {
-        const computerChoice = choice[Math.floor(Math.random()*3)];
+
+        // check condition_________________________________________________________________________
+        if (computerChoice === cRock) {
+            document.getElementById("result").textContent = "Both have Same Selection !";
+        } else if (computerChoice === cScissor) {
+            document.getElementById("result").textContent = "You have scored";
+            playerPoint += 1;
+        } else if (computerChoice === cPapper) {
+            document.getElementById("result").textContent = "Computer has scored";
+            computerPoint += 1;
+        }
+
+        updateScoreDisplay();
+    } else if (selection === "scissor") {
         computerChoice.classList.add("fa-beat");
-        pRock.classList.remove("fa-beat");
-        pPapper.classList.remove("fa-beat");
         pScissor.classList.add("fa-beat");
-    })
-);
-pPapper.addEventListener(
-    "click",
-    (move = () => {
-        const computerChoice = choice[Math.floor(Math.random()*3)];
+
+        if (computerChoice === cScissor) {
+            document.getElementById("result").textContent = "Both have Same Selection !";
+        } else if (computerChoice === cRock) {
+            document.getElementById("result").textContent = "Computer has scored";
+            computerPoint += 1;
+        } else if (computerChoice === cPapper) {
+            document.getElementById("result").textContent = "You have scored";
+            playerPoint += 1;
+        }
+
+        updateScoreDisplay();
+    } else if (selection === "papper") {
         computerChoice.classList.add("fa-beat");
-        pRock.classList.remove("fa-beat");
-        pScissor.classList.remove("fa-beat");
         pPapper.classList.add("fa-beat");
-    })
-);
+        if (computerChoice === cPapper) {
+            document.getElementById("result").textContent = "Both have Same Selection !";
+        } else if (computerChoice === cRock) {
+            document.getElementById("result").textContent = "You have scored";
+            playerPoint += 1;
+        } else if (computerChoice === cScissor) {
+            document.getElementById("result").textContent = "Computer has scored";
+            computerPoint += 1;
+        }
+        updateScoreDisplay();
+    }
+}
+
+function updateScoreDisplay() {
+    document.getElementById("scoreDisplayPlayer").textContent = playerPoint;
+    document.getElementById("scoreDisplayComputer").textContent = computerPoint;
+}
